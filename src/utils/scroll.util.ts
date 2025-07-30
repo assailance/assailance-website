7c5ad9dac515074f2aa24f2802eb47d7c135cbaa
assailance <intellligency@mail.ru>
2025-07-30 14:50:20 +0300

    refactor: плавный скролл к секциям переделан с js на css

--- src/utils/scroll.util.ts
+++
@@ -1,7 +0,0 @@
-export const goToSection = (sectionId: string) => {
-  const section = document.getElementById(sectionId) as HTMLElement
-  window.scrollTo({
-    top: section.offsetTop - 18 || 0,
-    behavior: 'smooth'
-  })
-}
