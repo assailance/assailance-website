dbcc4b1a82b968d974e3ff447b3ee8cadd6e9022
assailance <intellligency@mail.ru>
2025-08-15 21:52:28 +0300

    feat: добавлен основной компонент модального окна

--- src/components/ui/modal/Modal.vue
+++ src/components/ui/modal/Modal.vue
@@ -1,78 +1,39 @@
 <script setup lang="ts">
 import XIcon from '@/components/icons/XIcon.vue'
-import { onMounted, onUnmounted, ref, watch } from 'vue'
+import { useTemplateRef } from 'vue'
+import BaseModal from './BaseModal.vue'
 
 const model = defineModel<boolean>({ required: true })
 
-const app = document.getElementById('app') as HTMLElement
-
-const isVisible = ref<boolean>(false)
-const modalState = ref<'open' | 'closed'>('closed')
-
-const close = () => {
-  model.value = false
-}
-
-const onKeydown = (e: KeyboardEvent) => {
-  if (e.key === 'Escape') close()
-}
-
-onMounted(() => {
-  window.addEventListener('keydown', onKeydown)
-})
-
-onUnmounted(() => {
-  window.removeEventListener('keydown', onKeydown)
-})
-
-watch(model, value => {
-  if (value) {
-    const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth
-    document.body.style.overflow = 'hidden'
-    app.style.paddingRight = `${scrollbarWidth}px`
-    isVisible.value = true
-    modalState.value = 'open'
-  } else {
-    modalState.value = 'closed'
-    setTimeout(() => {
-      isVisible.value = false
-      app.style.removeProperty('padding-right')
-      document.body.style.removeProperty('overflow')
-    }, 135)
-  }
-})
+const baseModalRef = useTemplateRef<InstanceType<typeof BaseModal>>('baseModal')
 </script>
 
 <template>
-  <teleport to="#app">
-    <!-- Overlay -->
-    <div
-      v-if="isVisible"
-      :data-state="modalState"
-      class="data-[state=open]:animate-fade-in data-[state=closed]:animate-fade-out fixed inset-0 z-50 flex items-center justify-center bg-black/50"
-      @click.self="close"
-    ></div>
-    <!-- /Overlay -->
+  <BaseModal ref="baseModal" v-model="model">
     <!-- Modal -->
     <div
-      v-if="isVisible"
-      :data-state="modalState"
-      class="data-[state=open]:animate-modal-in border-border data-[state=closed]:animate-modal-out bg-global-bg fixed top-1/2 left-1/2 z-50 w-full max-w-lg -translate-x-1/2 -translate-y-1/2 rounded-xl border p-6 shadow-md"
+      v-if="baseModalRef?.isVisible"
+      :data-state="baseModalRef.modalState"
+      class="data-[state=open]:animate-modal-in border-border data-[state=closed]:animate-modal-out bg-global-bg fixed top-1/2 left-1/2 z-[9999] w-full max-w-lg -translate-x-1/2 -translate-y-1/2 rounded-xl border p-6 shadow-md"
       role="dialog"
       aria-modal="true"
       tabindex="-1"
     >
-      <slot></slot>
+      <!-- Content -->
+      <slot />
+      <!-- /Content -->
+      <!-- Close -->
       <button
         v-press-animate
         title="Close"
         class="text-global-text/85 hover:text-accent absolute top-4 right-4 transition-colors duration-200"
         aria-label="Close modal"
-        @click="close"
+        @click="baseModalRef.close()"
       >
         <XIcon />
       </button>
+      <!-- /Close -->
     </div>
     <!-- /Modal -->
-  </teleport>
+  </BaseModal>
 </template>
