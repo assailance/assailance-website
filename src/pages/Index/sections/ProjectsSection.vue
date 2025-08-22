99cfdec7d8396ee52e28c6d3d3d132e9a4275a0b
assailance <intellligency@mail.ru>
2025-08-22 21:43:44 +0300

    feat: добавлена секция с проектами

--- src/pages/Index/sections/ProjectsSection.vue
+++ src/pages/Index/sections/ProjectsSection.vue
@@ -1,7 +1,34 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { CardList, CardWithImage } from '@/components/ui/card-list'
+
+const skadeshopImages = [
+  '/skadeshop/1.webp',
+  '/skadeshop/2.webp',
+  '/skadeshop/3.webp',
+  '/skadeshop/4.webp',
+  '/skadeshop/5.webp',
+  '/skadeshop/6.webp',
+  '/skadeshop/7.webp',
+  '/skadeshop/8.webp',
+  '/skadeshop/9.webp',
+  '/skadeshop/10.webp'
+]
+</script>
 
 <template>
-    <h2 class="mb-4 text-xl">Projects</h2>
   <section id="projects" class="mt-20">
+    <h2 class="mb-4 text-xl">Projects</h2>
+    <!-- Projects -->
+    <CardList>
+      <CardWithImage
+        title="Skadeshop"
+        title-link="https://github.com/assailance"
+        description="А full-fledged trading platform for selling Minecraft accounts."
+        external-link="https://github.com/assailance"
+        :tags="['Python', 'Django', 'DRF', 'Vue', 'JavaScript', 'Tailwind', 'OAuth2']"
+        :images="skadeshopImages"
+      />
+    </CardList>
+    <!-- /Projects -->
   </section>
 </template>
