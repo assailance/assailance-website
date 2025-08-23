e1e6f1ebe138baf6cbf4ac6c47e3b9bd31e17bec
assailance <intellligency@mail.ru>
2025-08-23 16:12:18 +0300

    chore: добавлена __VUE_OPTIONS_API__=false опция

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  define: {
+    __VUE_OPTIONS_API__: 'false'
+  },
   server: {
     host: '127.0.0.1',
     port: 5173,
